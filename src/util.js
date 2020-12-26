2b23c4fdbbb46dc7b02cecc90ee7279d3bb35744
yfmei <15996213405@163.com>
2020-12-26 13:25:52 +0800

    object、 array check and define method

--- src/util.js
+++ src/util.js
@@ -3,6 +3,12 @@
  * @author yfmei
  * @date 2020/12/25
  */
+
+/**
+ * mix properties into target object
+ * @param target
+ * @param mixin
+ */
 function mixin(target, mixin) {
     for(let key in mixin){
         if (target[key] !== mixin[key]) {
@@ -11,16 +17,42 @@ function mixin(target, mixin) {
     }
 }
 
+/**
+ * 对象类型检测
+ * @param obj
+ * @returns {boolean}
+ */
 function isObject(obj) {
     return Object.prototype.toString.call(obj) === "[object Object]"
 }
 
+/**
+ * 数组类型检测
+ * @param arr
+ * @returns {arg is any[]}
+ */
 function isArray(arr) {
     return Array.isArray(arr)
+}
+
+/**
+ * 定义一个只读不可枚举的属性
+ * @param obj
+ * @param key
+ * @param value
+ */
+function define(obj, key, value) {
+    Object.defineProperty(obj, key, {
+        value,
+        enumerable: false,
+        writable: false,
+        configurable: true
+    })
 }
 
 export default {
     mixin,
     isArray,
-    isObject
+    isObject,
+    define
 }
